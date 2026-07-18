b77f2817fc87a13dc5458bdee65403bb6b1e808c
samuelankit <samuel.syndicate@gmail.com>
2026-07-18 15:26:20 +0100

    feat: add TikTok social icon (url editable in admin settings)

--- artifacts/portfolio/src/components/SocialIcons.tsx
+++ artifacts/portfolio/src/components/SocialIcons.tsx
@@ -16,6 +16,14 @@ function InstagramIcon() {
   );
 }
 
+function TikTokIcon() {
+  return (
+    <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19.59 6.69a4.83 4.83 0 01-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 01-2.88 2.5 2.89 2.89 0 01-2.89-2.89 2.89 2.89 0 012.89-2.89c.28 0 .54.04.79.1V9.01a6.33 6.33 0 00-.79-.05 6.34 6.34 0 00-6.34 6.34 6.34 6.34 0 006.34 6.34 6.34 6.34 0 006.33-6.34V8.69a8.19 8.19 0 004.79 1.53V6.77a4.85 4.85 0 01-1.02-.08z"/>
+    </svg>
+  );
+}
+
 function SpotlightIcon() {
   return (
     <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -30,10 +38,12 @@ export default function SocialIcons() {
   const instagram = settings?.instagramUrl ?? "";
   const facebook = settings?.facebookUrl ?? "";
   const spotlight = settings?.spotlightUrl ?? "";
+  const tiktok = settings?.tiktokUrl ?? "";
 
   const icons = [
     { url: facebook, label: "Facebook", Icon: FacebookIcon },
     { url: instagram, label: "Instagram", Icon: InstagramIcon },
+    { url: tiktok, label: "TikTok", Icon: TikTokIcon },
     { url: spotlight, label: "Spotlight", Icon: SpotlightIcon },
   ];
 
